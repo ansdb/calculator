7b27f00e6ee81b539f1dca7528c59619998b0737
Chidinma Godwin <gchidex@yahoo.com>
2019-12-21 14:06:17 +0100

    Handle the input display depending on the button clicked

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-let input = document.querySelector("#input-output-area");
+const input = document.querySelector("#input-output-area");
 
-// Add event listener to the buttons container 
-document.querySelector("#container").addEventListener("click", evt=> {
-    const target = evt.target;
-    if(target.classList.contains("btn")){
-        input.innerHTML = target.value;
-    }
-});
+// Function to display input
+(function inputDisplay() {
+
+    document.querySelector("#container").addEventListener("click", e => {
+      const value = e.target.value;
+      const targetClass = e.target.className;
+  
+      //  Check the class name of the clicked button
+      switch (targetClass) {
+        case "reset-btn":
+          input.innerHTML = value;
+          break;
+  
+        case "delete-btn":
+          // Delete the last input
+          input.innerHTML = input.innerHTML.slice(0, -1);
+          break;
+  
+        default:
+          // Clear the first "0" value that is initially displayed
+          input.innerHTML.charAt(0) == 0
+            ? (input.innerHTML = "")
+            : input.innerHTML;
+  
+          // Concatenate the values of the clicked button
+          input.innerHTML += value;
+          break;
+      }
+    });
+    
+  })();
